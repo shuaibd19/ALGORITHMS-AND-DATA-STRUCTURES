1bfb4685afe2bdbca1e02270d0cbd1d6a4bb2625
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-15 13:14:35 +1000

    Update ASS1main.cpp

--- ASS1main.cpp
+++ ASS1main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include "Read.h"
-#include "Write.h"
+#include "Ran_Write.h"
 #include <string>
 
 using namespace std;
 
 int main()
 {
-	//string iFileName = "input-a1q1.txt";
+	string iFileName = "input-a1q1.txt";
 	//keep this to the minimum
-	string oFileName = "output-a1q1.txt";
+	//string oFileName = "output-a1q1.txt";
 	Read read;
-	Write write;
-	write.writeToFile(oFileName);
-	read.readMyFile(oFileName);
+	Ran_Write write;
+	write.writeToFile(iFileName);
+	read.readMyFile(iFileName);
 	return 0;
 }
