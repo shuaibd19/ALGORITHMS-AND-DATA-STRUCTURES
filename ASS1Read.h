d9852cb82e89a32da011382076af84974b11cd01
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-15 13:15:01 +1000

    Update ASS1Read.h

--- ASS1Read.h
+++ ASS1Read.h
@@ -12,6 +12,11 @@ public:
 	Read();
 	~Read();
 	void readMyFile(string fileName);
+	bool isAsceding(int sortOrder);
+	bool isBestCase(int complexity);
+	void isBinary(int num);
+	void isPositive(int num);
+
 private:
 
 };
@@ -30,7 +35,7 @@ void Read::readMyFile(string fileName)
 	int sortOrder = -1;
 	int complexity = -1;
 	int sizeOfArray = -1;
-	//Remember to use pointers for your dinamic allocation
+	//Remember to use pointers for your dynamic allocation
 	int *items;
 	ifstream myfile(fileName);
 	if (myfile.is_open())
@@ -38,12 +43,15 @@ void Read::readMyFile(string fileName)
 		while (!myfile.eof())
 		{
 			myfile >> sortOrder;
+			isBinary(sortOrder);
 			cout << sortOrder << endl;
 			//condition for first if statement
 			myfile >> complexity;
+			isBinary(complexity);
 			cout << complexity << endl;
 			//condition for second if statement
 			myfile >> sizeOfArray;
+			isPositive(sizeOfArray);
 			cout << sizeOfArray << endl;
 			//condition for third if statement
 			items = new int[sizeOfArray];
@@ -56,8 +64,37 @@ void Read::readMyFile(string fileName)
 		}
 		myfile.close();
 	}
+	
 	else
 	{
-		cout << "Error: File Open for read";
+		cout << "Error: File Open for read\n";
+	}
+}
+
+bool Read::isAsceding(int sortOrder)
+{
+	return(sortOrder == 0);
+}
+
+bool Read::isBestCase(int complexity)
+{
+	return(complexity == 0);
+}
+
+void Read::isBinary(int num)
+{
+	if (num < 0 || num > 1)
+	{
+		cout << "Error: Number must be binary!\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Read::isPositive(int num)
+{
+	if (num < 0)
+	{
+		cout << "Error: Number cannot be negative!\n";
+		exit(EXIT_FAILURE);
 	}
 }
