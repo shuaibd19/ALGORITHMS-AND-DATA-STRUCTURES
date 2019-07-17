6bb57e53d3cfa2ff6e7c46125a5a412c78061886
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-17 14:29:41 +1000

    added bubble sort

--- ASS1main.cpp
+++ ASS1main.cpp
@@ -4,14 +4,18 @@
 #include "Save_Values.h"
 using namespace std;
 
+void printArray(int *items, int size);
+void bubbleSortA(int *arrayToSort, int size);
+void bubbleSortD(int *arrayToSort, int size);
+
 int main()
 {
 	int sortOrder = -2;
 	int complexity = -2;
 	int numOfItems = -2;
 	int *items;
-	//Gen_Ran_Inp random;
-	//random.writeRanInput();
+	Gen_Ran_Inp random;
+	random.writeRanInput();
 	
 	Save_Values save;
 	items = save.saveFromInput(&sortOrder, &complexity, &numOfItems);
@@ -19,9 +23,35 @@ int main()
 	cout << sortOrder << endl;
 	cout << complexity << endl;
 	cout << numOfItems << endl;
-	for (int i = 0; i < numOfItems; i++)
+	printArray(items, numOfItems);
+	cout << endl;
+	
+	if (sortOrder == 0)
 	{
-		if (i == (numOfItems - 1))
+		cout << endl;
+		cout << "Ascending order chosen:\n";
+		bubbleSortA(items, numOfItems);
+		printArray(items, numOfItems);
+		cout << endl;
+	}
+	else
+	{
+		cout << endl;
+		cout << "Descending order chosen:\n";
+		bubbleSortD(items, numOfItems);
+		printArray(items, numOfItems);
+		cout << endl;
+	}
+
+	cout << endl;
+	return 0;
+}
+
+void printArray(int *items, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (i == (size - 1))
 		{
 			cout << items[i];
 		}
@@ -30,6 +60,43 @@ int main()
 			cout << items[i] << " ";
 		}
 	}
-	cout << endl;
-	return 0;
+}
+
+//Reference to bubble sort algorithm: https://www.tutorialspoint.com/data_structures_algorithms/bubble_sort_program_in_c.htm
+//Also https://mathbits.com/MathBits/CompSci/Arrays/Bubble.htm
+void bubbleSortA(int *arrayToSort, int size)
+{
+	int temp = -1;
+	for (int i = 1; i <= size; i++)
+	{
+		for (int j = 0; j < size-1; j++)
+		{
+			if (arrayToSort[j+1] < arrayToSort[j])
+			{
+				temp = arrayToSort[j];
+				arrayToSort[j] = arrayToSort[j + 1];
+				arrayToSort[j + 1] = temp;
+			}
+		}
+	}
+}
+
+//Reference to bubble sort algorithm: https://www.tutorialspoint.com/data_structures_algorithms/bubble_sort_program_in_c.htm
+//Also https://mathbits.com/MathBits/CompSci/Arrays/Bubble.htm
+
+void bubbleSortD(int *arrayToSort, int size)
+{
+	int temp = -1;
+	for (int i = 1; i <= size; i++)
+	{
+		for (int j = 0; j < size - 1; j++)
+		{
+			if (arrayToSort[j + 1] > arrayToSort[j])
+			{
+				temp = arrayToSort[j];
+				arrayToSort[j] = arrayToSort[j + 1];
+				arrayToSort[j + 1] = temp;
+			}
+		}
+	}
 }
