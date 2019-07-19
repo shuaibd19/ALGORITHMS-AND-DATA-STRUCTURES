dc1a0876e4b685b300a16f0bcc3bb5f51954b800
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-20 03:25:10 +1000

    Update LinkedList.h

--- assignment1part2/LinkedList.h
+++ assignment1part2/LinkedList.h
@@ -13,47 +13,73 @@ class LinkedList
 public:
 	LinkedList();
 	~LinkedList();
-	void insertAtBeginning(int numOfElm);
+	void insertAtBeginning(int num);
 	void deleteFromBeginning();
-	void insertAtEnd(int numOfElm);
+	void insertAtEnd(int num);
 	void deleteFromEnd();
 	void displayNodes();
 
 private:
 	Node *First;
 	Node *Last;
-	Node *Current;
 };
 
 LinkedList::LinkedList()
 {
 	First = NULL;
 	Last = NULL;
-	Current = NULL;
-
 }
 
 LinkedList::~LinkedList()
 {
 }
 
-void LinkedList::insertAtBeginning(int numOfElm)
+void LinkedList::insertAtBeginning(int num)
 {
-
+	Node *temp = new Node;
+	temp->data = num;
+	if (First == NULL)
+	{
+		//This piece of code makes sure the linked list is not empty - 
+		//if the first node is null then this is the case -
+		//in such a case the first node is both the first and the last
+		//item on the linked list
+		First = temp;
+		Last = temp;
+	}
+	else
+	{
+		temp->next = First;
+		First = temp;
+	}
 }
 
 void LinkedList::deleteFromBeginning()
 {
-	
+	if (First == NULL)
+	{
+
+	}
+	else
+	{
+		Node *temp = new Node;
+		//Store the first node in temporary node
+		temp = First;
+		//The new first node is the one after the original
+		First = First->next;
+		//delete the temp node and hence the first node
+		delete temp;
+	}
+
 }
 
-void LinkedList::insertAtEnd(int numOfElm)
+void LinkedList::insertAtEnd(int num)
 {
 	//creating a new 'temp' node with the provided value going to 
 	//its data member. The node is being added to the end hence 
 	//it pointing to NULL
 	Node *temp = new Node;
-	temp->data = numOfElm;
+	temp->data = num;
 	temp->next = NULL;
 
 	if (First == NULL)
@@ -75,9 +101,20 @@ void LinkedList::insertAtEnd(int numOfElm)
 	}
 }
 
+//this node is problematic and crashing my program!!!!
 void LinkedList::deleteFromEnd()
 {
-	
+	Node *Current = new Node;
+	Node *Previous = new Node;
+	Current = First;
+	while (Current->next != NULL)
+	{
+		Previous = Current;
+		Current = Current->next;
+	}
+	Last = Previous;
+	Previous->next = NULL;
+	delete Current;
 }
 
 void LinkedList::displayNodes()
