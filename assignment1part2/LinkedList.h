eb8bcc4b69e65072d5a5e7ad3477ca9356da0414
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-20 03:25:24 +1000

    Update LinkedList.h

--- assignment1part2/LinkedList.h
+++ assignment1part2/LinkedList.h
@@ -1,129 +1,37 @@
 #pragma once
 #include <iostream>
+#include <time.h>
+#include <fstream>
+#include <math.h>
 using namespace std;
 
-struct Node
-{
-	int data;
-	Node *next;
-};
-
-class LinkedList
+class NumberGen
 {
 public:
-	LinkedList();
-	~LinkedList();
-	void insertAtBeginning(int num);
-	void deleteFromBeginning();
-	void insertAtEnd(int num);
-	void deleteFromEnd();
-	void displayNodes();
+	NumberGen();
+	~NumberGen();
+	void writeRanInp();
 
 private:
-	Node *First;
-	Node *Last;
+	long long int randomNumber;
 };
 
-LinkedList::LinkedList()
+NumberGen::NumberGen()
 {
-	First = NULL;
-	Last = NULL;
+	/*due to rand function having a max value i have to to do something smart*/
+	srand((unsigned)time(NULL));
+	//randomNumber = (int)pow((double)(rand() % 999999), 2.0);
+	randomNumber = 189000;
 }
 
-LinkedList::~LinkedList()
+NumberGen::~NumberGen()
 {
 }
 
-void LinkedList::insertAtBeginning(int num)
+void NumberGen::writeRanInp()
 {
-	Node *temp = new Node;
-	temp->data = num;
-	if (First == NULL)
-	{
-		//This piece of code makes sure the linked list is not empty - 
-		//if the first node is null then this is the case -
-		//in such a case the first node is both the first and the last
-		//item on the linked list
-		First = temp;
-		Last = temp;
-	}
-	else
-	{
-		temp->next = First;
-		First = temp;
-	}
-}
-
-void LinkedList::deleteFromBeginning()
-{
-	if (First == NULL)
-	{
-
-	}
-	else
-	{
-		Node *temp = new Node;
-		//Store the first node in temporary node
-		temp = First;
-		//The new first node is the one after the original
-		First = First->next;
-		//delete the temp node and hence the first node
-		delete temp;
-	}
-
-}
-
-void LinkedList::insertAtEnd(int num)
-{
-	//creating a new 'temp' node with the provided value going to 
-	//its data member. The node is being added to the end hence 
-	//it pointing to NULL
-	Node *temp = new Node;
-	temp->data = num;
-	temp->next = NULL;
-
-	if (First == NULL)
-	{
-		//This piece of code makes sure the linked list is not empty - 
-		//if the first node is null then this is the case -
-		//in such a case the first node is both the first and the last
-		//item on the linked list
-		First = temp;
-		Last = temp;
-	}
-	else
-	{
-		//In the case where there already is a node this makes that node 
-		//(known as 'Last') point to the new temp Node and makes the 
-		//temp node now the 'Last' node. Therefore inserting at end
-		Last->next = temp;
-		Last = Last->next;
-	}
-}
-
-//this node is problematic and crashing my program!!!!
-void LinkedList::deleteFromEnd()
-{
-	Node *Current = new Node;
-	Node *Previous = new Node;
-	Current = First;
-	while (Current->next != NULL)
-	{
-		Previous = Current;
-		Current = Current->next;
-	}
-	Last = Previous;
-	Previous->next = NULL;
-	delete Current;
-}
-
-void LinkedList::displayNodes()
-{
-	Node *temp = new Node;
-	temp = First;
-	while (temp != NULL)
-	{
-		cout << temp->data << ", ";
-		temp = temp->next;
-	}
+	ofstream myFile;
+	myFile.open("input-a1q2.txt");
+	myFile << randomNumber << endl;
+	myFile.close();
 }
