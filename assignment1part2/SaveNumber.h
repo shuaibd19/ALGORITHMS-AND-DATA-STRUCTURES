54a00c9b2465e661dad2bedb1d072d0d3b09002b
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-20 21:25:28 +1000

    Update SaveNumber.h

--- assignment1part2/SaveNumber.h
+++ assignment1part2/SaveNumber.h
@@ -8,6 +8,7 @@ class SaveNumber
 public:
 	SaveNumber();
 	~SaveNumber();
+	//Function looks for a number in a file and saves it to a memory address
 	void saveSize(int *numOfElm);
 };
 
@@ -21,15 +22,19 @@ SaveNumber::~SaveNumber()
 
 void SaveNumber::saveSize(int *numOfElm)
 {
+	//Look for a file with a specific name
 	ifstream myFile("input-a1q2.txt");
 	if (myFile.is_open())
 	{
+		//Checking to see if it reaches the end of the file
 		while (!myFile.eof())
 		{
+			//assign the integer found to the one provided in the parameters
 			myFile >> *numOfElm;
 		}
 		myFile.close();
 	}
+	//in the case of no file by that name being found report error
 	else
 	{
 		cout << "ERROR: No File found for reading Input\n";
