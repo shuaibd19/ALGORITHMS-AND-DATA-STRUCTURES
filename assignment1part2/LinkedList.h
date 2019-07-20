9661a8be6705bc6671bd81aaca6749884cc3bac1
shuaibd19 <51773730+shuaibd19@users.noreply.github.com>
2019-07-20 21:24:04 +1000

    Update LinkedList.h

--- assignment1part2/LinkedList.h
+++ assignment1part2/LinkedList.h
@@ -1,6 +1,8 @@
 /*references: https://www.codementor.io/codementorteam/a-comprehensive-guide-to-implementation-of-singly-linked-list-using-c_plus_plus-ondlm5azr
 https://www.geeksforgeeks.org/remove-last-node-of-the-linked-list/
-https://www.codesdope.com/blog/article/c-linked-lists-in-c-singly-linked-list/ */
+https://www.codesdope.com/blog/article/c-linked-lists-in-c-singly-linked-list/ 
+https://codeforwin.org/2015/09/c-program-to-delete-last-node-of-singly-linked-list.html 
+https://www.youtube.com/watch?time_continue=252&v=i3b3O7slIzk */
 
 #pragma once
 #include <iostream>
@@ -17,19 +19,25 @@ class LinkedList
 public:
 	LinkedList();
 	~LinkedList();
+	//Function to insert node at beginning of my linked list
 	void insertAtBeginning(int num);
+	//Function to delete node at beginning of my linked list
 	void deleteFromBeginning();
+	//Function to insert node at end of my linked list
 	void insertAtEnd(int num);
-	void deleteFromEnd();
+	//void deleteFromEnd();
+	//Function used to loop through linkedlist display all nodes
 	void displayNodes();
 
 private:
+	//Also known as head or tail nodes in Linked List terminology
 	Node *First;
 	Node *Last;
 };
 
 LinkedList::LinkedList()
 {
+	//Making sure they are not fed garbage values during compilation
 	First = NULL;
 	Last = NULL;
 }
@@ -40,6 +48,8 @@ LinkedList::~LinkedList()
 
 void LinkedList::insertAtBeginning(int num)
 {
+	//Using dynamic memory allocation to assign new Node temp and put int num
+	//inside its data member
 	Node *temp = new Node;
 	temp->data = num;
 	if (First == NULL)
@@ -53,6 +63,8 @@ void LinkedList::insertAtBeginning(int num)
 	}
 	else
 	{
+		//Making the temp point to the first and thus making it the first
+		//node
 		temp->next = First;
 		First = temp;
 	}
@@ -60,6 +72,8 @@ void LinkedList::insertAtBeginning(int num)
 
 void LinkedList::deleteFromBeginning()
 {
+	//If the first node is NUll then it's already empty so don't
+	//do anything
 	if (First == NULL)
 	{
 
@@ -106,23 +120,42 @@ void LinkedList::insertAtEnd(int num)
 }
 
 //this node is problematic and crashing my program!!!!
-void LinkedList::deleteFromEnd()
-{
-	Node *Current = new Node;
-	Node *Previous = new Node;
-	Current = First;
-	while (Current->next != NULL)
-	{
-		Previous = Current;
-		Current = Current->next;
-	}
-	Last = Previous;
-	Previous->next = NULL;
-	delete Current;
-}
+//void LinkedList::deleteFromEnd()
+//{
+//	Node *Current = new Node;
+//	Node *Previous = new Node;
+//
+//	if (First == NULL)
+//	{
+//
+//	}
+//	else
+//	{
+//		if (First->next == NULL)
+//		{
+//			Current = First;
+//			First = NULL;
+//			free(Current);
+//		}
+//		else
+//		{
+//			Current = First;
+//			while (Current->next != NULL)
+//			{
+//				Previous = Current;
+//				Current = Current->next;
+//			}
+//			Previous->next = NULL;
+//			free(Current);
+//		}
+//	}
+//}
 
 void LinkedList::displayNodes()
 {
+	//create a Node the will traverse through the linked list
+	//until reaching the  final node printing all the data members
+	//in the process and moving on to the next
 	Node *temp = new Node;
 	temp = First;
 	while (temp != NULL)
